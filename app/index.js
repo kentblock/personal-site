4ea1b036656947d68d1cbc991d518fb410459f72
Riley Peel <rpeel4@outlook.com>
2020-10-05 12:11:03 -0700

    cleaned up and ready just needs pictures

--- app/index.js
+++ app/index.js
@@ -7,33 +7,42 @@ const init = () => {
   const projectsPage = document.querySelector("#projects-page")
   const pages = [mainPage, projectsPage]
   const projectDisplay = document.querySelector("#project-display")
-  const linkToProjects = document.querySelector("#projects-link")
-  const navBarIcon = document.querySelector("#bars-icon")
-  const navBox = document.querySelector("#nav-box")
-  var settingNavBoxOpen
+  const projectsButton = document.querySelector("#projects-btn")
+  const homeButton = document.querySelector("#home-btn")
   var settingProjectDetailOpen
 
   const projects = {
     1: {
       title: "Chess Web App",
       image: "chessapp.png",
-      description: "A Chess App",
+      description: `This project is a web application where you can play chess against other users\
+        and track your performance. I wrote the logic, and the backend for the game in python, with the help\
+        of the Django web framework. I built out the front of the app with React and used Redux for state management.\
+        The code for this app, and a link to a live version can\
+        be found below.`,
       tech: "Django, React, Docker, AWS",
       github: "https://github.com/kentblock/chess",
-      link: "livesitelink"
+      link: "http://chess-app.xyz"
     },
     2: {
       title: "Stock Market Web App",
       image: "fakeimage.png",
-      description: "A Stonk App",
+      description: `This project is a web application which simulates buying and selling stocks, charts historical stock\
+        price data, and displays some fundamental company data. I built a RESTful API using Django, and the Django Rest Framework,\
+        as my backend for this app. I built out the frontend using Vue.js. You can find\
+        links to a live version and the code for this project below.`,
       tech: "Django, Vue, Docker, AWS",
-      github: "githublink",
-      link: "livesitelink"
+      github: "https://github.com/kentblock/stock-app",
+      link: "http://stock-app.xyz"
     },
     3: {
-      title: "MHSGV Mobile Resource App",
+      title: "MHSGV Mobile App",
       image: "fakeimage.png",
-      description: "A Mobile App",
+      description: `I am working under the guidance of a Senior Mobile Developer to help build a\
+        mobile app for the Mental Health Society of Greater Victoria. The app's main purpose is to\
+        provide users with a comprehensive list of mental health resources in the Victoria area. It\
+        also provides filters to help narrow down the resources best suited to a particular user. This app is still\
+        in development but the first version will be on the app store very shortly.`,
       tech: "iOS, Swift",
       github: null,
       link: null
@@ -41,27 +50,18 @@ const init = () => {
   }
   
   document.body.addEventListener('click', () => {
-    if (navBox.style.visibility == "visible" && !settingNavBoxOpen) {
-      navBox.style.visibility = "hidden"
-    }
     if (projectDisplay.style.visibility == "visible" && !settingProjectDetailOpen) {
       projectDisplay.style.visibility = "hidden"
     }
     settingProjectDetailOpen = false
-    settingNavBoxOpen = false
   })
 
-  navBarIcon.addEventListener('click', () => {
-    settingNavBoxOpen = true
-    console.log("clicked on bars")
-    console.log("what the hekc")
-    console.log(window.location.pathname)
-    navBox.style.visibility = "visible"
+  homeButton.addEventListener('click', () => {
+    goToPage(mainPage, HOME)
   })
 
-  linkToProjects.addEventListener('click', () => {
+  projectsButton.addEventListener('click', () => {
     goToPage(projectsPage, PROJECTS)
-    history.pushState({}, "", window.location.pathname)
   })
 
   window.addEventListener('popstate', (e) => {
@@ -116,14 +116,25 @@ const init = () => {
   const projectDescription = document.querySelector("#description")
   const projectImage = document.querySelector("#project-detail-img")
   const projectTitle = document.querySelector("#project-display-title")
-  const projectLinks = document.querySelector("#project-link")
+  const projectTech = document.querySelector("#project-tech")
+  const ghLink = document.querySelector("#project-gh-link")
+  const siteLink = document.querySelector("#project-site-link")
+
   const setProjectDisplay = id => {
     const project = projects[id]
     projectDescription.innerText = project.description
     projectImage.src = project.image
     projectTitle.innerText = project.title
+    projectTech.innerText = project.tech
+    ghLink.style.color = "white"
+    siteLink.style.color = "white"
     if (project.github) {
-      
+      ghLink.href = project.github
+      ghLink.style.color = "black"
+    }
+    if (project.link) {
+      siteLink.href = project.link
+      siteLink.style.color = "black"
     }
   }
   console.log(window.location.pathname)
